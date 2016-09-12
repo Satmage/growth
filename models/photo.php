633bdb343f07e19e01843543ab93c22c4ca4e80b
Satmage <kleckers@yandex.ru>
2016-09-12 18:34:50 +0300

    version 1.3 Изменение в функции Photo_getAll()

--- models/photo.php
+++ models/photo.php
@@ -5,21 +5,7 @@ require_once __DIR__ . '/../function/sql.php';
 function Photos_getAll() 
 {
 
-	sql_connect();
-	//Выборка данных из БД
+	
 	$sql = 'SELECT * FROM image';
-	//Заносим данные из БД в переменную
-	$res = mysql_query($sql);
-
-	$ret = [];
-
-	/*
-	Пока есть данные в БД записывать их в 
-	массив (с преобразованием типа объкта в массив)
-	*/
-	while (false !== ($row = mysql_fetch_assoc($res))) {
-		$ret[] = $row;
-	}
-
-	return $ret;
+	return sql_query($sql);
 }
