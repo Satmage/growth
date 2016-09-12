d1e96ce8e679950fa84e50ac319c56d84fbf785c
Satmage <kleckers@yandex.ru>
2016-09-12 17:32:17 +0300

    Построение MVC

---
+++ models/photo.php
@@ -0,0 +1,10 @@
+<?php
+
+function Photos_getAll() 
+{
+	return [
+			['title' => 'фото1', 'path' => '/img/photo1.jpg'],
+			['title' => 'фото2', 'path' => '/img/photo2.jpg'],
+			['title' => 'фото3', 'path' => '/img/photo3.jpg'],
+	];
+}
