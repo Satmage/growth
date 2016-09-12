96da3b3ed8c57878d42c19ac90e569f00b6bf5ad
Satmage <kleckers@yandex.ru>
2016-09-12 18:35:32 +0300

    Функция принимает запрос к БД

--- function/sql.php
+++ function/sql.php
@@ -5,4 +5,22 @@ function sql_connect()
 	mysql_select_db('gallery');
 }
 	
+function sql_query($sql)
+{
+	sql_connect();
+	//Заносим данные из БД в переменную
+	$res = mysql_query($sql);
+	$ret = [];
+	/*
+	Пока есть данные в БД записывать их в 
+	массив (с преобразованием типа объкта в массив)
+	*/
+	while (false !== ($row = mysql_fetch_assoc($res))) {
+		$ret[] = $row;
+	}
+
+	return $ret;
+	
+}
+
 ?>
